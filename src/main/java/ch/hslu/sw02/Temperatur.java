d41a2b2db003f07775322f729b9b1ab444ad1a80
flawas <32436687+flawas@users.noreply.github.com>
2022-03-06 13:03:32 +0100

    Temperatur class and TemperaturTest Class

---
+++ src/main/java/ch/hslu/sw02/Temperatur.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ch.hslu.sw02;
+
+/**
+ *
+ * @author flaviowaser
+ */
+public class Temperatur {
+    
+    /*
+    * Attributte 
+    */
+    private double tempCelsius;
+    private double tempKelvin;
+    private double tempFahrenheit;
+    private double relativeChange;
+    
+    public Temperatur(){
+        this.tempCelsius = 20;
+        tempFahrenheit = getTempFahrenheitFromCelsius(tempCelsius);
+        tempKelvin = getTempKelvinFromCelsius(tempCelsius);
+    }
+    
+    public Temperatur(double tempCelsius){
+        this.tempCelsius = tempCelsius;
+        tempFahrenheit = getTempFahrenheitFromCelsius(tempCelsius);
+        tempKelvin = getTempKelvinFromCelsius(tempCelsius);        
+    }
+    
+    public double getTempKelvinFromCelsius(double tempCelsius){
+        this.tempCelsius = tempCelsius;
+        tempKelvin = tempCelsius+273.15;
+        return tempKelvin;
+    }
+    
+    public double getTempFahrenheitFromCelsius(double tempCelsius){
+        this.tempCelsius = tempCelsius;
+        tempFahrenheit = tempCelsius * 1.8 +32;
+        return tempFahrenheit;
+    }
+    
+    public double getTempCelsiusFromFahrenheit(double tempFahrenheit){
+        this.tempFahrenheit = tempFahrenheit;
+        tempCelsius = tempFahrenheit / 1.8 - 32;
+        return tempCelsius;
+    }
+    
+    public double getTempCelsiusFromKelvin(double tempKelvin){
+        this.tempKelvin = tempKelvin;
+        tempCelsius = tempKelvin - 273.15;
+        return tempCelsius;
+    }
+
+    public double getTempCelsius() {
+        return tempCelsius;
+    }
+
+    public double getTempKelvin() {
+        return tempKelvin;
+    }
+
+    public double getTempFahrenheit() {
+        return tempFahrenheit;
+    }
+
+    public void setTempCelsius(double tempCelsius) {
+        this.tempCelsius = tempCelsius;
+    }
+    
+    public void setTempCelsiusRelative(double relativeChange){
+        this.relativeChange = relativeChange;
+        tempCelsius = tempCelsius + (relativeChange);
+        
+        // Umrechnen der anderen zwei Varianten
+        tempFahrenheit = getTempFahrenheitFromCelsius(tempCelsius);
+        tempKelvin = getTempKelvinFromCelsius(tempCelsius);
+    }
+    
+    public void setTempFahrenheitRelative(double relativeChange){
+        this.relativeChange = relativeChange;
+        tempFahrenheit = tempFahrenheit + (relativeChange);
+        
+        // Umrechnen der anderen zwei Varianten
+        tempCelsius = getTempCelsiusFromFahrenheit(tempFahrenheit);
+        tempKelvin = getTempKelvinFromCelsius(tempCelsius);
+    }
+    
+    public void setTempKelvinRelative(double relativeChange){
+        this.relativeChange = relativeChange;
+        tempKelvin = tempKelvin + (relativeChange);
+        
+        // Umrechnen der anderen zwei Varianten
+        tempCelsius = getTempCelsiusFromKelvin(tempKelvin);
+        tempFahrenheit = getTempFahrenheitFromCelsius(tempCelsius);
+    }
+    
+}
---
+++ src/test/java/ch/hslu/sw02/TemperaturTest.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ch.hslu.sw02;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author flaviowaser
+ */
+public class TemperaturTest {
+    
+    public TemperaturTest() {
+    }
+
+    @Test
+    public void testGetTemperatur() {
+        Temperatur temperatur = new Temperatur(20);
+        Assertions.assertEquals(20, temperatur.getTempCelsius());
+        
+    }
+    
+    @Test
+    public void testSetTemperatur(){
+        Temperatur temperatur = new Temperatur(20);
+        temperatur.setTempCelsius(32);
+        Assertions.assertEquals(32, temperatur.getTempCelsius());
+    }
+    
+    @Test
+    public void testGetTempKelvinFromCelsius(){
+        Temperatur temperatur = new Temperatur(10);
+        Assertions.assertEquals(283.15, temperatur.getTempKelvinFromCelsius(temperatur.getTempCelsius()));
+    }
+    
+    @Test
+    public void testGetTempFahrenheitFromCelsius(){
+        Temperatur temperatur = new Temperatur(15);
+        Assertions.assertEquals(59, temperatur.getTempFahrenheitFromCelsius(temperatur.getTempCelsius()));
+    }
+    
+    @Test
+    public void testSetTempCelsiusRelativePostivie(){
+        Temperatur temperatur = new Temperatur(8);
+        temperatur.setTempCelsiusRelative(2);
+        Assertions.assertEquals(10, temperatur.getTempCelsius());
+    }
+    
+    @Test
+    public void testSetTempCelsiusRelativeNegative(){
+        Temperatur temperatur = new Temperatur(8);
+        temperatur.setTempCelsiusRelative(-2);
+        Assertions.assertEquals(6, temperatur.getTempCelsius());
+    }
+    
+    @Test
+    public void testSetTempKelvinRelativePositive(){
+        Temperatur temperatur = new Temperatur(15);
+        temperatur.setTempKelvinRelative(20);
+        Assertions.assertEquals(35, temperatur.getTempCelsius());
+    }
+    
+    @Test 
+    public void testSetTempKelvinRelativeNegative(){
+        Temperatur temperatur = new Temperatur(32);
+        temperatur.setTempKelvinRelative(-20);
+        Assertions.assertEquals(12, temperatur.getTempCelsius());
+    }
+    
+    @Test
+    public void testSetTempFahrenheitRelativePositive(){
+        Temperatur temperatur = new Temperatur(15);
+        temperatur.setTempFahrenheitRelative(16);
+        Assertions.assertEquals(75, temperatur.getTempFahrenheit());
+    }
+    
+    
+    
+}
